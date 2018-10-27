4271d2400d94a5c6109b2f0d8dcce25d30922e7a
Gilson Leandro Polito <gilsonpolito@gmail.com>
2018-10-26 21:42:30 -0300

    Commit inicial

--- ListaTarefas/FirstViewController.swift
+++ ListaTarefas/FirstViewController.swift
@@ -8,8 +8,34 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tblTasks: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return taskMgr.tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCell.CellStyle.subtitle, reuseIdentifier: "test")
+        cell.textLabel?.text = taskMgr.tasks[indexPath.row].name
+        cell.detailTextLabel?.text = taskMgr.tasks[indexPath.row].desc
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCell.EditingStyle.delete{
+            taskMgr.tasks.remove(at: indexPath.row)
+            tblTasks.reloadData()
+        }
+    }
+    
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        tblTasks.reloadData()
+    }
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
--- ListaTarefas/SecondViewController.swift
+++ ListaTarefas/SecondViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    
+    @IBOutlet weak var txtText: UITextField!
+    @IBOutlet weak var txtDesc: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func btnAddTask(_ sender: Any) {
+        taskMgr.addTask(name: txtText.text!, desc: txtDesc.text!)
+        self.view.endEditing(true)
+        txtText.text = ""
+        txtDesc.text = ""
+        self.tabBarController?.selectedIndex = 0
+    }
 }
 
